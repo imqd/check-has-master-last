a77f2a4f4e7d7d883c13a8e1024979aafa458d0f
梁成 <sisheb49@gmail.com>
2020-07-05 02:08:51 +0800

    coding

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const shelljs = require('shelljs')
 const chalk = require('chalk')
 
-shelljs.exec('echo 2')
+function getExecSh (cli, params) {
+  shelljs.exec(
+    'echo 2',
+    {
+      silent: true
+    },
+    function (err, stdout, stderr) {
+      if (err) {
+        console.log('执行 sh 出错咯，快来看看！')
+      }
+      resolve(stdout)
+    }
+  )
+}
+
+function exec () {
+  const masterLastCommitId = getExecSh('git rev-parse origin/master')
+  const lastTag = getExecSh('git tag -l')
+  const lastTagCommitId = getExecSh(`git rev-parse ${lastTag}`)
+  if (masterLastCommitId !== lastTagCommitId) {
+    console.log(chalk.red('你大爷！你上次 tag 没打'))
+    exit(0)
+  }
+  const ary = getExecSh('git rev-parse origin/master')
+}
+
+
