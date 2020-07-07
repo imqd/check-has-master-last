7f2fe141fd184bf147d15f0016b5443d648b3b0c
梁成 <sisheb49@gmail.com>
2020-07-07 22:56:12 +0800

    bug: 修复项目首次开发没有tag的错误提示

--- index.js
+++ index.js
@@ -23,30 +23,39 @@ function getExecSh (cli, params) {
 async function exec () {
   const originMasterLastCommitId = await  getExecSh('git rev-parse origin/master')
   const lastTag = await getExecSh('git describe --tags origin/master')
-  const lastTagCommitId = await getExecSh(`git rev-parse ${lastTag}`)
-  // console.log('originMasterLastCommitId', originMasterLastCommitId)
-  // console.log('lastTag', lastTag)
-  // console.log('lastTagCommitId', lastTagCommitId)
+  if (lastTag) {
+    console.log(chalk.yellow('\n【目前，master 没有任何分支，确认下项目是不是初次开发】\n'))
+  }
+  const lastTagCommitId = await getExecSh(`git rev-parse ${getCleanTagName(lastTag)}`)
+  console.log('originMasterLastCommitId', originMasterLastCommitId)
+  console.log('lastTag', getCleanTagName(lastTag))
+  console.log('lastTagCommitId', lastTagCommitId)
   if (originMasterLastCommitId !== lastTagCommitId) {
     console.log(chalk.red('\n【小伙子，你很危险！】master 最新代码没有打 tag\n'))
     process.exit()
   }
-  // const currentBranchLastTag = await getExecSh('git describe --tags')
   const originMasterLastTag = await getExecSh('git describe --tags origin/master')
-  let currentBranchLastTag = await getExecSh(`git describe --tags`)
+  const currentBranchLastTag = await getExecSh(`git describe --tags`)
+  
+  // console.log('currentBranchLastTag', currentBranchLastTag)
+  // console.log('originMasterLastTag', originMasterLastTag)
+
+  if (getCleanTagName(currentBranchLastTag) !== getCleanTagName(originMasterLastTag)) {
+    console.log(chalk.red('\n【小伙子，你很危险！】你现在分支没有 master 最新代码\n'))
+    process.exitCode = 1
+  }
+  console.log(chalk.green('\n【代码已有 master 最新代码，很安全啊！】\n'))
+}
+
+function getCleanTagName (tag) {
   // https://www.yiibai.com/git/git_describe.html
   // git describe --tags
   // tag1-2-g026498b
   // 2:表示自打tag tag1 以来有2次提交(commit)
   // g026498b：g 为git的缩写，在多种管理工具并存的环境中很有用处；
-  currentBranchLastTag = currentBranchLastTag.slice(0, originMasterLastTag.length - 1)
-  // console.log('currentBranchLastTag', currentBranchLastTag)
-  // console.log('originMasterLastTag', originMasterLastTag)
-
-  if (currentBranchLastTag !== originMasterLastTag) {
-    console.log(chalk.red('\n【小伙子，你很危险！】你现在分支没有 master 最新代码\n'))
-    process.exitCode = 1
-  }
+  const cleanTag = tag.trim().replace(/\-\d{1,}\-g\w{1,}$/g, '')
+  console.log('cleantag', cleanTag)
+  return cleanTag
 }
 
 module.exports = exec
